aa33ab6fef3943ba116bff3e233bc30922a3882f
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2020-04-26 20:56:04 +0700

    DONE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 "use stricts";
+
+function magic() {
+    let name = prompt("👋 Hello! What's your name?");
+    let length = name.length;
+
+    if (length > 0) {
+        document.getElementById("name").innerHTML = "Hello " + name + ". welcome to my website";
+    }
+    else{
+        let name = alert("your name cannot be empty");
+        magic();
+    }
+}
