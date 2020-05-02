510aef604e8889721b5a1350ece288b2e4309a0f
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2020-05-02 22:58:10 +0700

    es5 to es6

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ function magic() {
     let length = name.length;
 
     if (length > 0) {
-        document.getElementById("name").innerHTML = "Hello " + name + ". welcome to my website";
+        document.getElementById("name").innerHTML = `Hello ${name}. welcome to my website`;
     }
     else{
         let name = alert("your name cannot be empty");
